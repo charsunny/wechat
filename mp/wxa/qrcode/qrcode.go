e6366fea0eba3d1c13727235c60fd8cdaf9d74b2
Xi Sun <charsunny@gmail.com>
2019-04-22 17:43:37 +0800

    fix bug

--- mp/wxa/qrcode/qrcode.go
+++ mp/wxa/qrcode/qrcode.go
@@ -77,12 +77,13 @@ func CreateWxaCode(clt *core.Client, path string, width int, lineColor *LineColo
 // auto_color	boolean	false	否	自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false
 // line_color	Object	{"r":0,"g":0,"b":0}	否	auto_color 为 false 时生效，
 // is_hyaline	boolean	false	否	是否需要透明底色，为 true 时，生成透明底色的小程序
-func CreateWxaCodeUnlimited(clt *core.Client, path string, width int, lineColor *LineColor, auto_color, is_hyaline bool) (data []byte, err error) {
+func CreateWxaCodeUnlimited(clt *core.Client, scene string, path string, width int, lineColor *LineColor, auto_color, is_hyaline bool) (data []byte, err error) {
 	const incompleteURL = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token="
 
 	var request = struct {
 		Path string `json:"path"`	//路径
 		Width int `json:"width"`	//宽度
+		Scene string `json:"scene"`
 		AutoColor bool `json:"auto_color"`
 		LineColor *LineColor `json:"line_color,omitempty"`
 		IsHyaline bool `json:"is_hyaline"`
@@ -91,6 +92,7 @@ func CreateWxaCodeUnlimited(clt *core.Client, path string, width int, lineColor 
 		Width:width,
 		AutoColor:auto_color,
 		LineColor:lineColor,
+		Scene: scene,
 		IsHyaline:is_hyaline,
 	}
 	var result struct {
