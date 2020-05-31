005ea9c311b17b3b40f77dd6f3121869b0dfd96f
Xi Sun <charsunny@gmail.com>
2020-06-01 00:15:00 +0800

    增加小程序红包接口

--- mch/mmpaymkttransfers/sendredpack.go
+++ mch/mmpaymkttransfers/sendredpack.go
@@ -9,3 +9,9 @@ import (
 func SendRedPack(clt *core.Client, req map[string]string) (resp map[string]string, err error) {
 	return clt.PostXML(core.APIBaseURL()+"/mmpaymkttransfers/sendredpack", req)
 }
+
+// 小程序红包发放.
+//  NOTE: 请求需要双向证书
+func SendMpRedPack(clt *core.Client, req map[string]string) (resp map[string]string, err error) {
+	return clt.PostXML(core.APIBaseURL()+"/mmpaymkttransfers/sendminiprogramhb", req)
+}
