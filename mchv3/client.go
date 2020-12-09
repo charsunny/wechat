b1057220c11ba70ff6f49f60ef07cd091d996e13
Xi Sun <charsunny@gmail.com>
2020-12-10 00:05:57 +0800

    fix bug

---
+++ mchv3/client.go
@@ -0,0 +1,264 @@
+package mchv3
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+/**
+  api v3 协议
+*/
+type Client struct {
+	// 是否是服务商支付
+	Isv bool
+	// 商户号
+	MerchantId string
+	// 证书序号
+	merchantCertificateSerialNo string
+	// 商户私钥
+	merchantPrivatekey *rsa.PrivateKey
+	//平台证书序号
+	flatCertificateSerialNo string
+	//平台证书-公钥
+	flatCertificate *rsa.PublicKey
+	Nonce           string
+	aes             *AesUtils
+	client          *http.Client
+}
+
+func NewClient(mchid, cno string, privatekey []byte) (client *Client) {
+	client = &Client{
+		Isv:        false,
+		MerchantId: mchid,
+	}
+	client.SetMerchantKey(cno, privatekey)
+	return
+}
+
+func NewIsvClient(mchid, cno string, privatekey []byte) (client *Client) {
+	client = &Client{
+		Isv:        true,
+		MerchantId: mchid,
+	}
+	client.SetMerchantKey(cno, privatekey)
+	return
+}
+
+//设置商户秘钥信息
+func (this *Client) SetMerchantKey(cno string, privatekey []byte) error {
+	this.merchantCertificateSerialNo = cno
+	block, _ := pem.Decode(privatekey)
+	if block == nil {
+		return fmt.Errorf("private key error!")
+	}
+	//解析PKCS1格式的私钥
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return err
+	}
+	this.merchantPrivatekey = priv.(*rsa.PrivateKey)
+	return nil
+}
+
+//设置平台秘钥信息
+func (this *Client) SetFlatKey(cno string, publickey []byte) error {
+	this.flatCertificateSerialNo = cno
+	block, _ := pem.Decode(publickey)
+	if block == nil {
+		return fmt.Errorf("public key error")
+	}
+	// 解析公钥
+	pubInterface, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return err
+	}
+	this.flatCertificate = pubInterface.PublicKey.(*rsa.PublicKey)
+
+	return nil
+}
+
+//设置Client的秘钥
+func (this *Client) SetClientKey(key string) {
+	if key != "" {
+		this.aes = &AesUtils{}
+		err := this.aes.Init(key)
+		if err != nil {
+			fmt.Println(err.Error())
+			this.aes = nil
+		}
+	}
+}
+func (this *Client) Call(method string, url string, parameter string) ([]byte, error) {
+	if this.client == nil {
+		this.client = &http.Client{}
+	}
+
+	req, err := http.NewRequest(method, url, strings.NewReader(parameter))
+	if err != nil {
+		return nil, err
+	}
+	//设置签名头
+	token := this.getToken(method, url, parameter)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", "WECHATPAY2-SHA256-RSA2048 "+token)
+
+	resp, err := this.client.Do(req)
+	if !this.validate(resp.Header) {
+		return nil, fmt.Errorf("微信平台返回的消息校验不通过，请检查网络是否被劫持！")
+	}
+	//if resp.StatusCode != http.StatusOK {
+	//	fmt.Println(resp)
+	//	return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", url, resp.StatusCode)
+	//}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+//检查消息头
+func (this *Client) validate(h http.Header) bool {
+	nonce := h["Wechatpay-Nonce"]
+	sign := h["Wechatpay-Signature"]
+	serial := h["Wechatpay-Serial"]
+	if this.aes != nil {
+		_, err := this.aes.DecryptToString(serial[0], nonce[0], sign[0])
+		if err != nil {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func (this *Client) DoGet(url string) ([]byte, error) {
+	return this.Call("GET", url, "")
+}
+
+func (this *Client) DoPost(url string, body string) ([]byte, error) {
+	return this.Call("POST", url, "")
+}
+
+func (this *Client) getToken(method string, url string, body string) string {
+	var t int64 = time.Now().Unix()
+	msg := this.buildSignatureMsg(method, url, t, body)
+	signer := this.sign(msg)
+	return fmt.Sprintf("mchid=\"%s\",nonce_str=\"%s\",timestamp=\"%d\",serial_no=\"%s\",signature=\"%s\"", this.MerchantId, this.Nonce, t, this.merchantCertificateSerialNo, signer)
+}
+
+func (this *Client) buildSignatureMsg(method string, urlstr string, timestamp int64, body string) string {
+	u, err := url.Parse(urlstr)
+	if err != nil {
+
+	}
+	canonicalUrl := u.RequestURI()
+	return fmt.Sprintf("%s\n%s\n%d\n%s\n%s\n", method, canonicalUrl, timestamp, this.Nonce, body)
+
+}
+
+func (this *Client) sign(msg string) string {
+	h := sha256.New()
+	h.Write([]byte(msg))
+	digest := h.Sum(nil)
+	s, err := rsa.SignPKCS1v15(rand.Reader, this.merchantPrivatekey, crypto.SHA256, digest)
+	if err != nil {
+		fmt.Println("rsaSign SignPKCS1v15 error", err.Error())
+		return ""
+	}
+	return base64.StdEncoding.EncodeToString(s)
+}
+
+/**
+  加密敏感信息
+*/
+func (this *Client) EncryptText(secretMessage string) (string, error) {
+	rng := rand.Reader
+	cipherdata, err := rsa.EncryptOAEP(sha1.New(), rng, this.flatCertificate, []byte(secretMessage), nil)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := base64.StdEncoding.EncodeToString(cipherdata)
+	return ciphertext, nil
+}
+
+/**
+  解密敏感信息
+*/
+func (this *Client) DecryptText(ciphertext string) (string, error) {
+	cipherdata, _ := base64.StdEncoding.DecodeString(ciphertext)
+	rng := rand.Reader
+
+	plaintext, err := rsa.DecryptOAEP(sha1.New(), rng, this.merchantPrivatekey, cipherdata, nil)
+	if err != nil {
+		return "", fmt.Errorf("Error from decryption: %s\n", err)
+	}
+
+	return string(plaintext), nil
+
+}
+
+//获取平台秘钥，并保存
+func (this *Client) DownloadFlatPublicKey(filename string) error {
+	data, err := this.DoGet("https://api.mch.weixin.qq.com/v3/certificates")
+	if err != nil {
+		return err
+	}
+	flatresponse := flatKeyResponse{}
+	json.Unmarshal(data, &flatresponse)
+	//保存文件
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	//循环遍历返回的秘钥，可能存在多个秘钥
+	for _, d := range flatresponse.Data {
+		f.WriteString(d.SerialNo)
+		f.WriteString("-------------------------------")
+		//解密文件
+		this.aes.DecryptToString(d.EncryptCertificate.AssociatedData, d.EncryptCertificate.Nonce, d.EncryptCertificate.Ciphertext)
+		f.WriteString("------------END-----------------")
+	}
+
+	return nil
+
+}
+
+//平台key返回结果对象
+type flatKeyResponse struct {
+	Data []certificateData
+}
+type certificateData struct {
+	SerialNo           string             `json:"serialNo"`
+	EffectiveTime      string             `json:"effectiveTime"`
+	ExpireTime         string             `json:"expireTime"`
+	EncryptCertificate encryptCertificate `json:"encrypt_certificate"`
+}
+
+type encryptCertificate struct {
+	Algorithm      string `json:"algorithm"`
+	Nonce          string `json:"nonce"`
+	AssociatedData string `json:"associated_data"`
+	Ciphertext     string `json:"ciphertext"`
+}
---
+++ mchv3/pay.go
@@ -0,0 +1,141 @@
+package mchv3
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+)
+
+const (
+	PayTypeAPP    = "app"
+	PayTypeJSAPI  = "jsapi"
+	PayTypeNative = "native"
+	PayTypeH5     = "h5"
+)
+
+type SceneInfo struct {
+	ClientIP  string     `json:"payer_client_ip"`
+	DeviceID  string     `json:"device_id,omitempty"`
+	StoreInfo *StoreInfo `json:"store_info,omitempty"`
+}
+
+type StoreInfo struct {
+	ID       string `json:"id"` // 公众号ID
+	Name     string `json:"name,omitempty"`
+	AreaCode string `json:"area_code,omitempty"`
+	Address  string `json:"address,omitempty"`
+}
+
+type PayDetail struct {
+	CostPrice *int64       `json:"cost_price,omitempty"`
+	InvoiceID string       `json:"invoice_id,omitempty"`
+	Goods     []*GoodsInfo `json:"goods_detail"`
+}
+
+type GoodsInfo struct {
+	ID        string `json:"merchant_goods_id"` // 公众号ID
+	WechatID  string `json:"wechatpay_goods_id,omitempty"`
+	GoodsName string `json:"goods_name,omitempty"`
+	Quantity  int64  `json:"quantity"`
+	UnitPrice int64  `json:"unit_price"`
+}
+
+type SettleInfo struct {
+	ProfitSharing bool   `json:"profit_sharing"`
+	SubsidyAmount *int64 `json:"subsidy_amount,omitempty"`
+}
+
+type PayOrder struct {
+	Type string `json:"-"`
+	// isv模式，除了sub app 均为必须
+	SpAppID  string `json:"sp_appid,omitempty"`  // 服务商appid
+	SpMchID  string `json:"sp_mchid,omitempty"`  // 服务商mchid
+	SubAppID string `json:"sub_appid,omitempty"` // 子账户appid
+	SubMchID string `json:"sub_mchid,omitempty"` // 子商户mchid
+	// 非isv模式，必须
+	AppID       string     `json:"appid,omitempty"` // 公众号ID
+	MchID       string     `json:"mchid,omitempty"`
+	Description string     `json:"description"`
+	OutTradeNo  string     `json:"out_trade_no"`
+	TimeExpire  *time.Time `json:"time_expire,omitempty"`
+	Attach      string     `json:"attach,omitempty"`
+	NotifyURL   string     `json:"notify_url"`
+	GoodsTag    string     `json:"goods_tag,omitempty"`
+	Amount      struct {
+		Total    int64  `json:"total"`
+		Currency string `json:"currency,omitempty"`
+	} `json:"amount"`
+	SettleInfo *SettleInfo `json:"settle_info,omitempty"`
+	PayDetail  *PayDetail  `json:"detail,omitempty"`
+	SceneInfo  *SceneInfo  `json:"scene_info,omitempty"`
+}
+
+type CombineSubOrder struct {
+	MchID       string `json:"mchid"`
+	SubMchID    string `json:"sub_mchid"`
+	Description string `json:"description"`
+	OutTradeNo  string `json:"out_trade_no"`
+	Attach      string `json:"attach"`
+	Amount      struct {
+		Total    int64  `json:"total"`
+		Currency string `json:"currency,omitempty"`
+	} `json:"amount"`
+	SettleInfo *SettleInfo `json:"settle_info,omitempty"`
+}
+
+type CombinePayOrder struct {
+	Type       string             `json:"-"`
+	AppID      string             `json:"combine_appid,omitempty"` // 服务商appid
+	MchID      string             `json:"combine_mchid,omitempty"` // 服务商mchid
+	OutTradeNo string             `json:"combine_out_trade_no"`
+	SceneInfo  *SceneInfo         `json:"scene_info,omitempty"`
+	SubOrders  []*CombineSubOrder `json:"sub_orders"`
+	PayInfo    struct {
+		OpenID string `json:"openid,omitempty"`
+	} `json:"combine_payer_info,omitempty"`
+	TimeStart  *time.Time `json:"time_start,omitempty"`
+	TimeExpire *time.Time `json:"time_expire,omitempty"`
+	NotifyURL  string     `json:"notify_url"`
+}
+
+func Transaction(v3 *Client, order PayOrder) (result map[string]string, err error) {
+	partner := ""
+	if v3.Isv {
+		if order.SpAppID == "" || order.SpMchID == "" || order.SubMchID == "" {
+			err = errors.New("商户号缺失，无法下单")
+			return
+		}
+		order.AppID = ""
+		order.MchID = ""
+		partner = "partner/"
+	} else {
+		if order.AppID == "" || order.MchID == "" {
+			err = errors.New("商户号缺失，无法下单")
+			return
+		}
+		order.SpAppID = ""
+		order.SpMchID = ""
+		order.SubMchID = ""
+		order.SubAppID = ""
+	}
+	url := fmt.Sprintf("https://api.mch.weixin.qq.com/v3/pay/%stransactions/%s", partner, order.Type)
+	data, _ := json.Marshal(order)
+	body, err := v3.DoPost(url, string(data))
+	if err != nil {
+		return
+	}
+	json.Unmarshal([]byte(body), &result)
+	return
+}
+
+func CombineTransaction(v3 *Client, order CombinePayOrder) (result map[string]string, err error) {
+	url := fmt.Sprintf("https://api.mch.weixin.qq.com/v3/combine-transactions/%s", order.Type)
+	data, _ := json.Marshal(order)
+	body, err := v3.DoPost(url, string(data))
+	if err != nil {
+		return
+	}
+	json.Unmarshal([]byte(body), &result)
+	return
+}
